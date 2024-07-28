83a78e62e94d563a518523a7e5d074a592ed6ead
vBoltunov <vitaliy.boltunov@yandex.ru>
2024-07-28 17:45:00 +0300

    Create fields and constructor in TaskManager class

--- src/infrastructure/TaskManager.java
+++ src/infrastructure/TaskManager.java
@@ -1,4 +1,24 @@
 package infrastructure;
 
+import model.Epic;
+import model.Subtask;
+import model.Task;
+
+import java.util.HashMap;
+
 public class TaskManager {
+    protected HashMap<Integer, Task> tasks;
+    protected HashMap<Integer, Epic> epics;
+    protected HashMap<Integer, Subtask> subtasks;
+
+    protected int taskId = 0;
+    protected int epicId = 0;
+    protected int subtaskId = 0;
+
+
+    public TaskManager() {
+        this.tasks = new HashMap<>();
+        this.epics = new HashMap<>();
+        this.subtasks = new HashMap<>();
+    }
 }
--- src/model/Epic.java
+++ src/model/Epic.java
@@ -2,7 +2,6 @@ package model;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 public class Epic extends Task {
     private final List<Integer> subtasks = new ArrayList<>();
