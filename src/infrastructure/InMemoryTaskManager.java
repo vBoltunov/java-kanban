50ed67b5683aa09f06ac46887e1937b2d3013a9c
vBoltunov <vitaliy.boltunov@yandex.ru>
2024-08-16 20:09:36 +0300

    Rewrite getters to add tasks to history

--- src/infrastructure/InMemoryTaskManager.java
+++ src/infrastructure/InMemoryTaskManager.java
@@ -91,17 +91,32 @@ public class InMemoryTaskManager implements TaskManager {
     // Получение по идентификатору
     @Override
     public Task getTaskById(int id) {
-        return tasks.get(id);
+        Task task = tasks.get(id);
+        if (task == null) {
+            return null;
+        }
+        historyManager.add(task);
+        return task;
     }
 
     @Override
     public Epic getEpicById(int id) {
-        return epics.get(id);
+        Epic epic = epics.get(id);
+        if (epic == null) {
+            return null;
+        }
+        historyManager.add(epic);
+        return epic;
     }
 
     @Override
     public Subtask getSubtaskById(int id) {
-        return subtasks.get(id);
+        Subtask subtask = subtasks.get(id);
+        if (subtask == null) {
+            return null;
+        }
+        historyManager.add(subtask);
+        return subtask;
     }
 
     // Создание задач
