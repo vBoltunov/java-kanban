c2b4df9da8d9cd52ebff8310c59b56a8fb5e4013
vBoltunov <vitaliy.boltunov@yandex.ru>
2024-08-16 19:50:27 +0300

    Create new getDefaultHistory() method in Managers class

--- src/infrastructure/Managers.java
+++ src/infrastructure/Managers.java
@@ -5,4 +5,8 @@ public class Managers {
     public static TaskManager getDefault() {
         return null;
     }
+
+    public static HistoryManager getDefaultHistory() {
+        return new InMemoryHistoryManager();
+    }
 }
