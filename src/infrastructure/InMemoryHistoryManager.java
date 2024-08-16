4340ca66e93e008fb3e87ba38af093d9b84a9b29
vBoltunov <vitaliy.boltunov@yandex.ru>
2024-08-16 19:55:12 +0300

    Create add() method body for adding tasks to history

--- src/infrastructure/InMemoryHistoryManager.java
+++ src/infrastructure/InMemoryHistoryManager.java
@@ -10,7 +10,12 @@ public class InMemoryHistoryManager implements HistoryManager {
     private final List<Task> taskHistory = new ArrayList<>(10);
 
     @Override
-    public void add(Task task) {}
+    public void add(Task task) {
+        if (taskHistory.size() == 10) {
+            taskHistory.removeFirst();
+        }
+        taskHistory.add(task);
+    }
 
     @Override
     public List<Task> getHistory() {
