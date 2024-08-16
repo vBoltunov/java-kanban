77e2ae8c0615452860ca5ac6f16a5e3ec5c54dce
vBoltunov <vitaliy.boltunov@yandex.ru>
2024-08-16 19:48:21 +0300

    Create class 'InMemoryHistoryManager' and its' methods

---
+++ src/infrastructure/InMemoryHistoryManager.java
@@ -0,0 +1,17 @@
+package infrastructure;
+
+import model.Task;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InMemoryHistoryManager implements HistoryManager {
+
+    @Override
+    public void add(Task task) {}
+
+    @Override
+    public List<Task> getHistory() {
+        return null;
+    }
+}
