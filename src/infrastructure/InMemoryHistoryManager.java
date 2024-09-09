f67855684f96525f650171ac16a7e0d478ed49c1
vBoltunov <vitaliy.boltunov@yandex.ru>
2024-09-09 13:06:47 +0300

    Add new hashmap into InMemoryHistoryManager to store history records

--- src/infrastructure/InMemoryHistoryManager.java
+++ src/infrastructure/InMemoryHistoryManager.java
@@ -1,11 +1,18 @@
 package infrastructure;
 
+import model.Node;
 import model.Task;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class InMemoryHistoryManager implements HistoryManager {
+
+    private final Map<Integer, Node<Task>> taskHistoryMap = new HashMap<>();
+    private Node<Task> head;
+    private Node<Task> tail;
 
     private final List<Task> taskHistory = new ArrayList<>(10);
 
