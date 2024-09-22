c3177dc26f1f265b6fc3a39c1e5c4145ae9e6e46
vBoltunov <vitaliy.boltunov@yandex.ru>
2024-09-22 11:59:04 +0300

    Create save() method in FileBackedTaskManager

--- src/infrastructure/FileBackedTaskManager.java
+++ src/infrastructure/FileBackedTaskManager.java
@@ -1,6 +1,10 @@
 package infrastructure;
 
+import exceptions.ManagerSaveException;
+
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class FileBackedTaskManager extends InMemoryTaskManager implements TaskManager {
 
@@ -9,5 +13,22 @@ public class FileBackedTaskManager extends InMemoryTaskManager implements TaskMa
     public FileBackedTaskManager(HistoryManager historyManager, File file) {
         super(historyManager);
         this.file = file;
+    }
+
+    protected void save() {
+        try (FileWriter writer = new FileWriter(file)) {
+            writer.write("id,type,name,status,description,epic\n");
+            for (Integer key : tasks.keySet()) {
+                writer.write(tasks.get(key).toString() + "\n");
+            }
+            for (Integer key : epics.keySet()) {
+                writer.write(epics.get(key).toString() + "\n");
+            }
+            for (Integer key : subtasks.keySet()) {
+                writer.write(subtasks.get(key).toString() + "\n");
+            }
+        } catch (IOException exp) {
+            throw new ManagerSaveException("Произошла ошибка записи в файл", exp);
+        }
     }
 }
