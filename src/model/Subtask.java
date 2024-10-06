5b61c5ef34d9b4b6ab4f95d9ed85261ebb5fbf70
vBoltunov <vitaliy.boltunov@yandex.ru>
2024-10-06 18:29:47 +0300

    Override equals and hashCode methods in Subtask class

--- src/model/Subtask.java
+++ src/model/Subtask.java
@@ -5,6 +5,7 @@ import model.enums.TaskType;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 public class Subtask extends Task {
     private final int epicId;
@@ -37,6 +38,20 @@ public class Subtask extends Task {
 
     public TaskType getType() {
         return TaskType.SUBTASK;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Subtask subtask = (Subtask) o;
+        return epicId == subtask.epicId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), epicId);
     }
 
     @Override
