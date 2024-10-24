9ac0658daaa5ee3a201efad005249317465a3801
vBoltunov <vitaliy.boltunov@yandex.ru>
2024-10-24 23:26:00 +0300

    Managers: add getGson() method

--- src/managers/Managers.java
+++ src/managers/Managers.java
@@ -1,5 +1,13 @@
 package managers;
 
+import adapters.DurationAdapter;
+import adapters.LocalDateTimeAdapter;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
 public class Managers {
 
     private static TaskManager taskManager;
@@ -17,5 +25,12 @@ public class Managers {
 
     public static HistoryManager getDefaultHistory() {
         return new InMemoryHistoryManager();
+    }
+
+    public static Gson getGson() {
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        gsonBuilder.registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter());
+        gsonBuilder.registerTypeAdapter(Duration.class, new DurationAdapter());
+        return gsonBuilder.create();
     }
 }
